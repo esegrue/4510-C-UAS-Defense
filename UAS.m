485d857e2df0ede29837e1a8b50e8f7bf1aba336
abenzigar1 <benzigar.1@osu.edu>
2026-03-12 17:52:27 -0400

    Update UAS.m

--- UAS.m
+++ UAS.m
@@ -14,7 +14,6 @@ classdef UAS < handle
         altitude
         turnRadius
 
-        adversary_path
         pathPoints
         pathHeadings
         tickOffset
@@ -23,12 +22,7 @@ classdef UAS < handle
     end
 
     methods
-        function obj = UAS(speed, entrance, target, mode, altitude, turnRadius, options)
-            arguments
-                speed, entrance, target, mode, altitude, turnRadius
-                options.adversary_path = [];
-
-            end
+        function obj = UAS(speed, entrance, target, mode, altitude, turnRadius)
             obj.speed = speed;
             obj.altitude = altitude;
             obj.turnRadius = turnRadius;
@@ -36,20 +30,15 @@ classdef UAS < handle
             obj.target = target;
             obj.mode = mode;
             obj.tempSpeed = speed;
-            dir2D = obj.target(1:2) - obj.position(1:2);
-            obj.targetUnitVector = [dir2D/norm(dir2D), 0]; % Z-component is 0
-            obj.active = true; % Default to active
 
-            obj.adversary_path = options.adversary_path; %please note that this will have both ingress and egress
+            dir2D = obj.target(1:2) - obj.position(1:2);
+            obj.targetUnitVector = [dir2D/norm(dir2D), 0];
+
+            obj.active = true;
             obj.pathPoints = [];
             obj.tickOffset = 0;
             obj.planner = [];
             obj.heading = atan2(obj.targetUnitVector(2), obj.targetUnitVector(1));
-
-            if ~isempty(obj.adversary_path)
-                obj.pathPoints = obj.adversary_path(:,1:2);
-                obj.pathHeadings = obj.adversary_path(:,3);
-            end
         end
 
         function linearMotion(obj, time)
@@ -57,118 +46,62 @@ classdef UAS < handle
                 obj.position = obj.position + obj.speed*time*obj.targetUnitVector;
             end
         end
+
         function hybridAStarMotion(obj, time, tick, turnRadius, costMap)
             if obj.active
-                if isempty(obj.planner) & isempty(obj.adversary_path) %i.e. we haven't passed a pre-planned path in
+                if isempty(obj.planner)
+
                     ss = stateSpaceSE2;
                     ss.StateBounds = [costMap.XWorldLimits; costMap.YWorldLimits; -pi pi];
+
                     sv = validatorOccupancyMap(ss);
                     sv.Map = costMap;
-                    %show(costMap)
-                    obj.planner = plannerHybridAStar(sv, 'MinTurningRadius', turnRadius, "InterpolationDistance",obj.speed*time);
-                    % Plan initial path
-                    refPath = plan(obj.planner, [obj.position(1:2), obj.heading], [obj.target, obj.heading+pi/2]);
-                    obj.pathPoints = refPath.States(:, 1:2);  % Just x,y coordinates
+
+                    obj.planner = plannerHybridAStar( ...
+                        sv,'MinTurningRadius',turnRadius, ...
+                        "InterpolationDistance",obj.speed*time);
+
+                    refPath = plan(obj.planner, ...
+                        [obj.position(1:2), obj.heading], ...
+                        [obj.target, obj.heading+pi/2]);
+
+                    obj.pathPoints = refPath.States(:,1:2);
                     obj.pathHeadings = refPath.States(:,3);
-                    pts = refPath.States; % [x y theta]
-                    fprintf("WARNING: Generating paths in-loop. Consider pre-generating for speed.")
                 end
-    
-                if tick-obj.tickOffset~=0
-                    if (tick - obj.tickOffset) <= length(obj.pathPoints(:,1))
-                        pose = obj.pathPoints(tick - obj.tickOffset,:);
-                        obj.position = [pose(1), pose(2), obj.position(3)];
-                        obj.heading = obj.pathHeadings(tick - obj.tickOffset);
-                        
-      
-                    elseif isempty(obj.adversary_path) %reached end of path, now escape (only need to generate if no pre-gen path)
+
+                if tick-obj.tickOffset ~= 0
+                    if (tick-obj.tickOffset) <= length(obj.pathPoints(:,1))
+
+                        pose = obj.pathPoints(tick-obj.tickOffset,:);
+                        obj.position = [pose(1),pose(2),obj.position(3)];
+                        obj.heading = obj.pathHeadings(tick-obj.tickOffset);
+
+                    else
+
                         positionxy = [obj.position(1), obj.position(2)];
-                        posEsc = [costMap.XWorldLimits(1), obj.position(2); obj.position(1), costMap.YWorldLimits(1); costMap.XWorldLimits(2), obj.position(2); obj.position(1), costMap.YWorldLimits(2)];
-                        [~, Iesc] = min(sum((posEsc - positionxy).^2, 2));
-                        obj.target = posEsc(Iesc, :);
-                        refPath = plan(obj.planner, [positionxy, obj.heading], [obj.target, obj.heading]);
-                        obj.pathPoints = refPath.States(:, 1:2);  % Just x,y coordinates
-                        pts = refPath.States; % [x y theta]
-                        %plot(pts(:,1), pts(:,2), 'g--', 'LineWidth', 2);
-                        obj.tickOffset = tick-1;
-                        pose = obj.pathPoints(tick - obj.tickOffset,:);
-                        obj.position = [pose(1), pose(2), obj.position(3)];
+
+                        posEsc = [ ...
+                            costMap.XWorldLimits(1), obj.position(2)
+                            obj.position(1), costMap.YWorldLimits(1)
+                            costMap.XWorldLimits(2), obj.position(2)
+                            obj.position(1), costMap.YWorldLimits(2)];
+
+                        [~,Iesc] = min(sum((posEsc-positionxy).^2,2));
+                        obj.target = posEsc(Iesc,:);
+
+                        refPath = plan(obj.planner, ...
+                            [positionxy,obj.heading], ...
+                            [obj.target,obj.heading]);
+
+                        obj.pathPoints = refPath.States(:,1:2);
                         obj.pathHeadings = refPath.States(:,3);
+
+                        obj.tickOffset = tick-1;
+
+                        pose = obj.pathPoints(tick-obj.tickOffset,:);
+                        obj.position = [pose(1),pose(2),obj.position(3)];
                     end
-                else
-                    obj.position = obj.position;
                 end
-            end
-        end
-
-        function searchMotion(obj, time, asset, isAssetDestroyed, NFZs)
-            if ~obj.active
-                return;
-            end
-            
-            obj.range = 20;
-            obj.obstacles.NFZs = NFZs;
-            obj.obstacles.asset = asset;
-            
-            % avoidNFZ logic needs to be fixed to actually turn the UAS
-            obj.avoidNFZ(); 
-
-            if ~isAssetDestroyed
-                dist2D = norm(obj.position(1:2) - asset.location(1:2));
-                
-                if dist2D <= obj.range
-                    obj.assetFound(dist2D, asset.location, time);
-                else
-                    obj.position = obj.position + obj.speed*time*obj.targetUnitVector;
-                end
-            end
-        end
-
-        function assetFound(obj, assetDistance, assetLocation, time)
-            turnRadius = assetDistance/2;
-            
-            assetVector = [assetLocation, 0] - [obj.position(1:2), 0];
-            
-            tuv = [obj.targetUnitVector(1:2), 0];
-            
-            turnAngle = acos(dot(tuv, assetVector)/(norm(tuv)*norm(assetVector)));
-            rotDir = cross(tuv, assetVector);
-            
-            angleVelo = (sin(turnAngle)*obj.speed)/turnRadius;
-            angle = angleVelo*time;
-
-            if abs(turnAngle) > 0.1
-                obj.turnMotion(angle, rotDir, time);
-            else
-                obj.position = obj.position + obj.tempSpeed*time*obj.targetUnitVector;
-            end
-        end
-
-        function turnMotion(obj, angle, rotDir, time)
-            if rotDir(3) < 0
-                DCM = [cos(angle) -sin(angle); sin(angle) cos(angle)];
-            elseif rotDir(3) > 0
-                DCM = [cos(-angle) -sin(-angle); sin(-angle) cos(-angle)];
-            else
-                DCM = eye(2);
-            end
-            
-            newVec2D = (DCM * obj.targetUnitVector(1:2)')';
-            obj.targetUnitVector = [newVec2D, 0];
-            
-            obj.position = obj.position + obj.tempSpeed*time*obj.targetUnitVector;
-        end
-
-        function avoidNFZ(obj)
-            % detects but does not yet turn
-            if isinterior(obj.obstacles.NFZs, obj.position + obj.targetUnitVector*obj.range)
-                angle = linspace(-pi/4, pi/4, 100);
-                options = zeros(100, 2);
-                for n = 1:length(angle)
-                    check = obj.position' + [cos(-angle(n)) -sin(-angle(n)); sin(-angle(n)) cos(-angle(n))]*obj.targetUnitVector'*obj.range;
-                    options(n, :) = check';
-                end
-                crash = find(isinterior(obj.obstacles.NFZs, options) == true);
             end
         end
     end
