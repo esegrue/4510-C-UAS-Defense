b689d02b3bb6cd2357cf1e60df02607147f38895
abenzigar1 <benzigar.1@osu.edu>
2026-03-12 17:46:03 -0400

    Update ingressPosns.m

--- ingressPosns.m
+++ ingressPosns.m
@@ -1,50 +1,189 @@
-function [pos] = ingressPosns(xlim, ylim, numUAS)
-    % Randomly generates positions of all UAS ingressions along the map
-    % perimeter
+function pos = ingressPosns(xlim, ylim, numUAS, edges, minSep)
 
-    % Inputs:
-    % xlim: [a,b] width of map
-    % ylim: [c,d] height of map
-    % numUAS: umber of UAS in a scenario (N)
+    % Randomly generates UAS ingress positions along the map perimeter
+    %
+    % Inputs
+    % xlim: [xmin xmax]
+    % ylim: [ymin ymax]
+    % numUAS: number of adversaries
+    % edges: "ALL","DISTRIBUTE","BOTTOM","RIGHT","TOP","LEFT"
+    % minSep: minimum separation between ingress points
 
-    % Output:
-    % pos: [N x 2] (x1,y1; x2,y2;...;xN,yN)
-    
     arguments
         xlim (1,2) double
         ylim (1,2) double
-        numUAS (1,1) double = 1 % default 1 adversary
+        numUAS (1,1) double = 1
+        edges (1,1) string = "ALL"
+        minSep (1,1) double = 0
     end
-    
-    x_min = xlim(1); x_max = xlim(2);
-    y_min = ylim(1); y_max = ylim(2);
 
-    width = x_max - x_min;
-    height = y_max - y_min;
-    perimeter_len = 2 * width + 2 * height;
+    edges = upper(string(edges));
 
-    % Generate N random distances along the perimeter
-    d = rand(numUAS, 1) * perimeter_len;
-    pos = nan(numUAS, 2);
+    xmin = xlim(1);
+    xmax = xlim(2);
+    ymin = ylim(1);
+    ymax = ylim(2);
 
-    % Vectorized Mapping
-    % 1. Bottom Edge
-    idx = d < width;
-    pos(idx, 1) = x_min + d(idx);
-    pos(idx, 2) = y_min;
+    width = xmax - xmin;
+    height = ymax - ymin;
 
-    % 2. Right Edge
-    idx = (d >= width) & (d < (width + height));
-    pos(idx, 1) = x_max;
-    pos(idx, 2) = y_min + (d(idx) - width);
+    pos = nan(numUAS,2);
 
-    % 3. Top Edge
-    idx = (d >= (width + height)) & (d < (2*width + height));
-    pos(idx, 1) = x_max - (d(idx) - (width + height));
-    pos(idx, 2) = y_max;
+    if minSep > 0
 
-    % 4. Left Edge
-    idx = d >= (2*width + height);
-    pos(idx, 1) = x_min;
-    pos(idx, 2) = y_max - (d(idx) - (2*width + height));
+        count = 0;
+        attempts = 0;
+        maxAttempts = 2000;
+
+        while count < numUAS && attempts < maxAttempts
+
+            attempts = attempts + 1;
+
+            edgeId = pickEdge(edges);
+
+            p = samplePoint(edgeId,xmin,xmax,ymin,ymax);
+
+            if count > 0
+
+                d = vecnorm(pos(1:count,:) - p,2,2);
+
+                if any(d < minSep)
+                    continue
+                end
+
+            end
+
+            count = count + 1;
+
+            pos(count,:) = p;
+
+        end
+
+        if count < numUAS
+            pos = pos(1:count,:);
+        end
+
+        return
+
+    end
+
+    if edges == "DISTRIBUTE"
+
+        base = randperm(4);
+
+        for i = 1:numUAS
+
+            edgeId = base(mod(i-1,4)+1);
+
+            pos(i,:) = samplePoint(edgeId,xmin,xmax,ymin,ymax);
+
+        end
+
+        return
+
+    end
+
+    if edges == "ALL"
+
+        perimeterLen = 2*width + 2*height;
+
+        d = rand(numUAS,1) * perimeterLen;
+
+        idx = d < width;
+
+        pos(idx,1) = xmin + d(idx);
+        pos(idx,2) = ymin;
+
+        idx = (d >= width) & (d < width + height);
+
+        pos(idx,1) = xmax;
+        pos(idx,2) = ymin + (d(idx) - width);
+
+        idx = (d >= width + height) & (d < 2*width + height);
+
+        pos(idx,1) = xmax - (d(idx) - (width + height));
+        pos(idx,2) = ymax;
+
+        idx = d >= (2*width + height);
+
+        pos(idx,1) = xmin;
+        pos(idx,2) = ymax - (d(idx) - (2*width + height));
+
+    else
+
+        edgeId = pickEdge(edges);
+
+        switch edgeId
+
+            case 1
+                pos(:,1) = xmin + rand(numUAS,1)*width;
+                pos(:,2) = ymin;
+
+            case 2
+                pos(:,1) = xmax;
+                pos(:,2) = ymin + rand(numUAS,1)*height;
+
+            case 3
+                pos(:,1) = xmin + rand(numUAS,1)*width;
+                pos(:,2) = ymax;
+
+            case 4
+                pos(:,1) = xmin;
+                pos(:,2) = ymin + rand(numUAS,1)*height;
+
+        end
+
+    end
+
+end
+
+function edgeId = pickEdge(edges)
+
+    if edges == "ALL" || edges == "DISTRIBUTE"
+        edgeId = randi(4);
+        return
+    end
+
+    switch edges
+
+        case "BOTTOM"
+            edgeId = 1;
+
+        case "RIGHT"
+            edgeId = 2;
+
+        case "TOP"
+            edgeId = 3;
+
+        case "LEFT"
+            edgeId = 4;
+
+        otherwise
+            error("Invalid edge selection")
+
+    end
+
+end
+
+function p = samplePoint(edgeId,xmin,xmax,ymin,ymax)
+
+    width = xmax - xmin;
+    height = ymax - ymin;
+
+    switch edgeId
+
+        case 1
+            p = [xmin + rand()*width , ymin];
+
+        case 2
+            p = [xmax , ymin + rand()*height];
+
+        case 3
+            p = [xmin + rand()*width , ymax];
+
+        case 4
+            p = [xmin , ymin + rand()*height];
+
+    end
+
 end
